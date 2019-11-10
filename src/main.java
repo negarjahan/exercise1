6f3b656e4da884a40588ddd882f6369ac898ffae
negar jahanbakht <njahanbakht@yahoo.com>
2019-11-10 10:43:07 +0330

    first commit

---
+++ src/main.java
@@ -0,0 +1,13 @@
+/**
+ * Created by asus on 9/21/2019.
+ */
+public class main {
+
+        public static void main(String args[]){
+            int i=10;
+            do{
+                System.out.println(i*2);
+                i--;
+            }while(i>=5);
+        }
+}
